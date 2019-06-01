b320cfe92c8ab6347e874db6f3d7e5d219e94160
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-01 21:00:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,12 +6,12 @@ def array_with_two_elements
   [1,2]
 end
 
-def first_element
-  my_first_element = [@taylor_swift,"john roach"]
+def first_element(my_first_element)
+  my_first_element = [@taylor_swift]
   my_first_element[0]
 end
 
-def third_element
+def third_element(my_third_element)
   my_third_element = ["Rock Lee","john roach", "@taylor_swift"]
   my_third_element[2]
 end
