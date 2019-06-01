58aae08c034b99bfe01df063be10609813c7ab87
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-01 20:58:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,12 @@
 def instantiate_new_array
   []
 end
+
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element
+  my_first_element = [@taylor_swift,"john roach"]
+  my_first_element[0]
+end
